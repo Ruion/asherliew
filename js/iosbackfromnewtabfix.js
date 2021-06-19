57b08e15927733e657a7e7b09b315ef1389b7dde
asher <asher_liew@uid.asia>
2021-06-19 20:20:22 +0800

    - ios safari show continue panel whenever pagehide event trigger

--- js/iosbackfromnewtabfix.js
+++ js/iosbackfromnewtabfix.js
@@ -11,7 +11,23 @@ window.IOS = function() {
   || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
 }
 
-window.onblur = iosLoseFocusToNewTabAction();
+// window.onblur = iosLoseFocusToNewTabAction();
+
+if(window.IOS())
+{
+  window.addEventListener("pagehide", event => {
+    if (event.persisted) {
+      /* the page isn't being discarded, so it can be reused later */
+      if((`#continue-panel`).length > 0) return;
+        $(`body`).append(`
+          <div id="continue-panel" style="position: fixed; width:100vw; height: 100vh; z-index: 999999; background: black; top: 0;
+          left: 0;">
+              <div id="continue-btn" onclick="destroyContinuePanel()" style="display:block; cursor:pointer">CONTINUE</div>
+          </div>
+        `)
+    }
+  }, false);
+}
 
 function iosLoseFocusToNewTabAction(){
     if(window.IOS() == true){
