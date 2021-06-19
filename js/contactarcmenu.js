f12e5b3cccb29aa44232a7e0fdaf8f4046818c0a
asher <asher_liew@uid.asia>
2021-06-19 21:21:23 +0800

    - contact menu change to click expand and click shrink

--- js/contactarcmenu.js
+++ js/contactarcmenu.js
@@ -1,3 +1,5 @@
+var contactMenuExpand = false;
+
 $(document).ready(function(){
   // console.log($(`#arc-menu-contact object`).contents().find(`[id=arc-expand-group]`));
     $('#arc-menu-contact').load(`img/arcmenucontact.svg`,
@@ -16,8 +18,18 @@ $(document).ready(function(){
           'transition': 'all 0.25s ease'
       });
 
-      $(`${parentId} .arc-hold-active`).on('mouseenter touchstart', (event) => {
-        onPress(parentId);
+      $(`${parentId} .arc-hold-active`).on('click', (event) => {
+      // $(`${parentId} .arc-hold-active`).on('mouseenter touchstart', (event) => {
+        if(!contactMenuExpand)
+          {
+            contactMenuExpand = true;
+            onPress(parentId);
+          }
+          else 
+          {
+          contactMenuExpand = false;
+          onRelease(parentId);
+        }
       });
 
       $(`${parentId} .arc-icon`).each(function(){ 
@@ -50,8 +62,8 @@ $(document).ready(function(){
         onRelease(parentId);
      });
 
-      $(this).on('mouseleave touchend', (event) => {
-          onRelease(parentId);
-      });
+      // $(this).on('mouseleave touchend', (event) => {
+      //     onRelease(parentId);
+      // });
     });
 });
